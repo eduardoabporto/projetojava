6487a6e1d2eca58dd81d13df7761acd2bae4e2ff
eduardoabporto <eduardo.porto@innovasys.com.br>
2022-01-26 16:09:54 -0300

    Criação do Projeto

---
+++ projetojava/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiroClasseJava {
+	
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto Console*/
+		System.out.println("Meu Primeiro Programa em Java");
+	}
+}
