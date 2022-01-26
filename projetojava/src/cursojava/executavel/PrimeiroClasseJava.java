5ffa43512dc1d80eef1a308b405422930746e8a0
eduardoabporto <eduardo.porto@innovasys.com.br>
2022-01-26 16:23:48 -0300

    teste2

--- projetojava/src/cursojava/executavel/PrimeiroClasseJava.java
+++ projetojava/src/cursojava/executavel/PrimeiroClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiroClasseJava {
 	
-	/*Main é um metodo auto executavel em Java*/
+	/*Main é um metodo auto executável em Java*/
 	public static void main(String[] args) {
 		
 		/*Escreve texto Console*/
